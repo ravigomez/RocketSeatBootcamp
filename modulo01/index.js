ec9e1f982d069af91e1e8ed2813ee6cf19c2bae8
Fernando H. Zaghi <fernando.zaghi@cubotecnologia.com.br>
2019-06-25 20:23:49 -0300

    continuacao modulo 1

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,9 +2,41 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-  console.log('teste');
-  
+server.use(express.json());
+
+const users = ['Fernando', 'Diego', 'BatataAAAA'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
 });
 
 server.listen(3000)
